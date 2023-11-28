f61a3f9a717a7511103aa0b5a584a9fff9e33d64
sunrab1822 <wbarnus99@gmail.com>
2023-11-28 09:38:14 +0100

    asd

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap'
+import './assets/style.scss'
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
